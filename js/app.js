1e69c658a911801658cd6673a0be440c03dc272c
RenatoArenas <renato.arenas2012@gmail.com>
2022-02-02 17:40:53 -0500

    Agregada funcionalidad de keyup a los input, y poder cambiar de aumento a descuento sin que se modifiquen los valores de los input

--- js/app.js
+++ js/app.js
@@ -7,21 +7,39 @@ document.getElementById("discounts-link").addEventListener('click', discountsBtn
 
 const increases_link = document.getElementById("increases-link");
 const discounts_link = document.getElementById("discounts-link");
-const increase_btn = document.getElementById("increase");
-const discount_btn = document.getElementById("discount");
+const increase_num = document.getElementById("numberA");
+const discount_num= document.getElementById("numberD");
+const increase_per = document.getElementById("percentageA");
+const discount_per = document.getElementById("percentageD");
 
 //Active Links && Change buttons
 
 function increasesBtn() {
     increases_link.classList.add("button-active");
     discounts_link.classList.remove("button-active");
-    increase_btn.classList.add("button-block");
-    discount_btn.classList.remove("button-block");
+    increase_num.classList.add("input-block");
+    discount_num.classList.remove("input-block");
+    increase_per.classList.add("input-block");
+    discount_per.classList.remove("input-block");
+    if (increase_num.value == 0) {    
+        increase_num.value = "";
+    }
+    if (increase_per.value == 0) {
+        increase_per.value = "";
+    }
 }
 
 function discountsBtn() {
     increases_link.classList.remove("button-active");
     discounts_link.classList.add("button-active");
-    increase_btn.classList.remove("button-block");
-    discount_btn.classList.add("button-block");
+    increase_num.classList.remove("input-block");
+    discount_num.classList.add("input-block");
+    increase_per.classList.remove("input-block");
+    discount_per.classList.add("input-block");
+    if (discount_num.value == 0) {
+        discount_num.value = "";
+    }
+    if (discount_per.value == 0) {
+        discount_per.value = "";
+    }
 }
--- js/calcs.js
+++ js/calcs.js
@@ -1,7 +1,10 @@
 //Event Listeners
-
-document.getElementById("increase").addEventListener('click', showIncreases);
-document.getElementById("discount").addEventListener('click', showDiscounts);
+document.getElementById("increases-link").addEventListener('click', showIncreases);
+document.getElementById("discounts-link").addEventListener('click', showDiscounts);
+document.getElementById("numberA").addEventListener('keyup', showIncreases);
+document.getElementById("numberD").addEventListener('keyup', showDiscounts);
+document.getElementById("percentageA").addEventListener('keyup', showIncreases);
+document.getElementById("percentageD").addEventListener('keyup', showDiscounts);
 
 //Variables
 
@@ -22,14 +25,38 @@ function discounts(number, percentage) {
 //Show Result
 
 function showIncreases() {
-    let number = Number(document.getElementById("number").value);
-    let percentage = Number(document.getElementById("percentage").value);
+    const discount_num = document.getElementById("numberD");
+    const discount_per = document.getElementById("percentageD");
+    let number = Number(document.getElementById("numberA").value);
+    let percentage = Number(document.getElementById("percentageA").value);
     increases(number, percentage);
-    result.textContent = "Resultado: " + res;
+    result.innerHTML = "Resultado: " + res;
+    discount_num.value = number;
+    discount_per.value = percentage;
+    if (increase_num.value == 0) {    
+        increase_num.value = "";
+        result.innerHTML = "Resultado: ";
+    }
+    if (increase_per.value == 0) {
+        increase_per.value = "";
+        result.innerHTML = "Resultado: ";
+    }
 }
 function showDiscounts() {
-    let number = Number(document.getElementById("number").value);
-    let percentage = Number(document.getElementById("percentage").value);
+    const increase_num = document.getElementById("numberA");
+    const increase_per = document.getElementById("percentageA"); 
+    let number = Number(document.getElementById("numberD").value);
+    let percentage = Number(document.getElementById("percentageD").value);
     discounts(number, percentage);
-    result.textContent = "Resultado: " + res;
+    result.innerHTML = "Resultado: " + res;
+    increase_num.value = number;
+    increase_per.value = percentage;
+    if (discount_num.value == 0) {
+        discount_num.value = "";
+        result.innerHTML = "Resultado: ";
+    }
+    if (discount_per.value == 0) {
+        discount_per.value = "";
+        result.innerHTML = "Resultado: ";
+    }
 }
